b16905592ff151e80746b9c56a3ebcd8a569d4ed
Victor <victor19vargas2018@gmail.com>
2026-09-14 15:20:29 -0500

    refactor(backend): actualizar configuración del servidor en server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 
 const app = require('./app');
+const connectDatabase = require('./config/database');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(
-    `\n🚀 Servidor corriendo en el entorno: ${process.env.NODE_ENV || 'development'}`
-  );
-  console.log(`🌐 Backend disponible en: http://localhost:${PORT}\n`);
-});
+const startServer = async () => {
+  await connectDatabase();
+
+
+  app.listen(PORT, () => {
+    console.log(
+      `\n🚀 Servidor corriendo en el entorno: ${process.env.NODE_ENV || 'development'}`
+    );
+    console.log(`🌐 Backend disponible en: http://localhost:${PORT}\n`);
+  });
+};
+
+startServer();
